9761fb31ddc968a44e94c1f366d94fb916c97cee
ghostWood1071 <thinhquangshin@gmail.com>
2023-09-12 16:21:21 +0700

    update extract

--- models/kafka_consumer_event.py
+++ models/kafka_consumer_event.py
@@ -65,7 +65,7 @@ class KafkaConsumer_event_class:
         start_date, end_date = get_day_in_week()
         request = requests.post(settings.EXTRACT_API, params={"id":  message['id_new'], "start_date": start_date, "end_date": end_date})
         if not request.ok:
-            raise Exception("can not extract event")
+            print("can not extract event")
         # i = 0
         # f = open('/home/ds1/vosint/v-osint-backend/vosint_ingestion/time_log_events.txt','a')
         # f.write(str(i)+ ' '+str(time.time()-start_time)+'\n')
